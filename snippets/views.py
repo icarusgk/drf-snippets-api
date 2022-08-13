3b13a05c7bce64375b6e51f866544dc890514a7f
Roger <38413630+icarusgk@users.noreply.github.com>
2022-08-13 13:25:01 -0600

    feat: DRF Part 3: Class Based Views

--- snippets/views.py
+++ snippets/views.py
@@ -1,69 +1,102 @@
 # from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+# from django.views.decorators.csrf import csrf_exempt
 # from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import status
-from rest_framework.decorators import api_view
+from django.http import Http404
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 # Listing all the existing snippets or creating a new snippet
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetList(APIView):
   """
   List all code snippets, or create a new snippet
   """
-  if request.method == 'GET':
+
+  def get(self, request, format=None):
     snippets = Snippet.objects.all()
     serializer = SnippetSerializer(snippets, many=True)
     return Response(serializer.data)
 
-  elif request.method == 'POST':
-    # Instead of parsing the binary data form the resquest
-    # data = JSONParser().parse(request)
-
-    # The @api_view nourishes the request and does that for us
+  def post(self, request, format=None):
     serializer = SnippetSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      # With rest_framework status we get access to more detailed
-      # status codes
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+  # if request.method == 'GET':
+  #   snippets = Snippet.objects.all()
+  #   serializer = SnippetSerializer(snippets, many=True)
+  #   return Response(serializer.data)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+  # elif request.method == 'POST':
+  #   # Instead of parsing the binary data form the resquest
+  #   # data = JSONParser().parse(request)
+
+  #   # The @api_view nourishes the request and does that for us
+  #   serializer = SnippetSerializer(data=request.data)
+  #   if serializer.is_valid():
+  #     serializer.save()
+  #     # With rest_framework status we get access to more detailed
+  #     # status codes
+  #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+  #   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
   """
   Retrieve, update or delete a code snippet
   """
-  try:
-    # Get the snippet
-    snippet = Snippet.objects.get(pk=pk)
-  except Snippet.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
 
-  if request.method == 'GET':
+  def get_object(self, pk):
+    try:
+      # Get the snippet
+      return Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+      raise Http404
+
+  def get(self, request, pk, format=None):
+    snippet = self.get_object(pk)
     serializer = SnippetSerializer(snippet)
+    # Return the data as JSON
     return Response(serializer.data)
 
-  elif request.method == 'PUT':
-    # Part 1. Parse the binary data from the request
-    # As a native python datatypes 
-    # data = JSONParser().parse(request)
-
-    # Replace the data from the snipped with the new received data
+  def put(self, request, pk, format=None):
+    snippet = self.get_object(pk)
     serializer = SnippetSerializer(snippet, data=request.data)
-    # If the parsed data received my the serializer is valid
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-  elif request.method == 'DELETE':
+  def delete(self, request, pk, format=None):
+    snippet = self.get_object(pk)
     snippet.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+  # if request.method == 'GET':
+  #   serializer = SnippetSerializer(snippet)
+  #   return Response(serializer.data)
+
+  # elif request.method == 'PUT':
+  #   # Part 1. Parse the binary data from the request
+  #   # As a native python datatypes 
+  #   # data = JSONParser().parse(request)
+
+  #   # Replace the data from the snipped with the new received data
+  #   serializer = SnippetSerializer(snippet, data=request.data)
+  #   # If the parsed data received my the serializer is valid
+  #   if serializer.is_valid():
+  #     serializer.save()
+  #     return Response(serializer.data)
+  #   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  # elif request.method == 'DELETE':
+  #   snippet.delete()
+  #   return Response(status=status.HTTP_204_NO_CONTENT)
     
     
